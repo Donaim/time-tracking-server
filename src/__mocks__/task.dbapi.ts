07b1a21c93fcd69d99148ead7ddfa8b266807b33
Donaim <3Donaim@gmail.com>
2021-06-26 03:44:11 +0200

    Add more API tests

--- src/__mocks__/task.dbapi.ts
+++ src/__mocks__/task.dbapi.ts
@@ -1,14 +1,28 @@
 import * as T from '../task.types';
+import { getDynamic } from '../dynamicScope';
 
-export async function startTask() {
+export async function startTask(title: string, description: string | null) {
+  const hook = getDynamic('startTaskHook');
+  if (hook) {
+    return hook(title, description);
+  }
   return null;
 }
 
 export async function stopTask() {
+  const hook = getDynamic('stopTaskHook');
+  if (hook) {
+    return hook();
+  }
   return T.DbStopTaskStatus.OK;
 }
 
 export async function getCurrentTask() {
+  const hook = getDynamic('getCurrentTaskHook');
+  if (hook) {
+    return hook();
+  }
+
   const ret: T.TaskDbEntry = {
     task: {
       title: 'testtitle',
